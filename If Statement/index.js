ebed8fe2c1e5b8449816379cba3eb21323350b5a
OlaiwonAbdullahi <132976400+OlaiwonAbdullahi@users.noreply.github.com>
2024-07-24 16:21:05 +0100

    new folder

--- If Statement/index.js
+++ If Statement/index.js
@@ -14,7 +14,7 @@ if (time < 12) {
     console.log("good morning")   
 } else {
     console.log("good Afternoon")
-}*/
+}
 
 let isStudent = false;
 
@@ -22,4 +22,30 @@ if (isStudent) {
     console.log("you are a student")    
 } else {
     console.log("you are not a student")
+}*/
+
+const myText = document.getElementById("myText")
+const mySubmit = document.getElementById("mySubmit")
+const resultElement = document.getElementById("resultElement")
+let age;
+
+mySubmit.onclick = function(){
+    age = myText.value;
+    age = Number(age);
+    
+    if (age >= 100) {
+        resultElement.textContent =`you're too old to enter this site`   
+    }
+     else if(age == 0) {
+        resultElement.textContent = `you're just born`
+    }
+    else if(age > 18) {
+        resultElement.textContent = `you're old enough to enter this site`
+    }
+    else if(age < 0) {
+        resultElement.textContent = `you're age can't be less than 0`
+    }
+    else {
+        resultElement.textContent = `you must be 18+ to enter this site`
+    }
 }
