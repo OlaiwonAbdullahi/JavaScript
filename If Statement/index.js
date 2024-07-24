c1ebc007395cff674b5fdfeda49acac71682c709
OlaiwonAbdullahi <132976400+OlaiwonAbdullahi@users.noreply.github.com>
2024-07-24 15:15:07 +0100

    changes

---
+++ If Statement/index.js
@@ -0,0 +1,25 @@
+/*let age = 25;
+
+if (age >= 18) {
+    console.log("you're old enough to enter this site");
+
+    
+} else {
+    console.log("you must be 18+ to enter this site")
+}*/
+
+/*let time = 14;
+
+if (time < 12) {
+    console.log("good morning")   
+} else {
+    console.log("good Afternoon")
+}*/
+
+let isStudent = false;
+
+if (isStudent) {
+    console.log("you are a student")    
+} else {
+    console.log("you are not a student")
+}
