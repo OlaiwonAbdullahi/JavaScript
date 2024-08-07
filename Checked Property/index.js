ef09aafa6039d0170620f93cd1990908907e0864
OlaiwonAbdullahi <132976400+OlaiwonAbdullahi@users.noreply.github.com>
2024-08-07 15:46:15 +0100

    new topic

---
+++ Checked Property/index.js
@@ -0,0 +1,26 @@
+const myCheckBox = document.getElementById("myCheckBox");
+const visaBtn = document.getElementById("visaBtn");
+const masterCardBtn = document.getElementById("masterCardBtn");
+const mySubmit = document.getElementById("mySubmit");
+const subResult = document.getElementById("subResult");
+const payPalBtn = document.getElementById("payPalBtn");
+const paymentResult = document.getElementById("paymentResult");
+
+mySubmit.onclick = function(){
+
+    if(myCheckBox.checked){
+        subResult.textContent = `you Just Subscribed`;
+        
+    }
+    else{
+         subResult.textContent = `you are not a  Subscriber`;
+    }
+    if (visaBtn.checked) {
+        paymentResult.textContent = `you're paying with visa`
+
+    }
+    else if(masterCardBtn.checked){
+        paymentResult.textContent = `you're paying with MasterCard`
+
+    }
+}
